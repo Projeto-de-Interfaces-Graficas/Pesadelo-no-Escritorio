2fe324207cd023bd0a0ec50dff65f453a92d817f
Daniel Henrique Oliveira <odanielhenrique646@gmail.com>
2025-09-24 20:20:56 -0300

    Add structure for enemies

---
+++ Enemy.c
@@ -0,0 +1,39 @@
+#include "Enemy.h"
+
+Enemy enemiesActive[MAX_ENEMIES];
+
+void Enemy_SpawnEnemy(Enemy* enemy, int enemyType, int enemyPositionX, int enemyPositionY, SDL_Texture* image) {
+    enemy->x = enemyPositionX;
+    enemy->y = enemyPositionY;
+    enemy->active = 1;
+    enemy->type = enemyType;
+    enemy->sprite = image;
+
+    switch (enemyType) {
+        case 0: // Pencil
+            enemy->hp = 20; enemy->dmg = 5; enemy->def = 20; enemy->spd = 2;
+            enemy->w = 16; enemy->h = 16;
+            break;
+        case 1: // Telephone
+        case 2: // Printer
+        case 3: // Adhesive Tape
+        case 4: // Stapler
+        case 5: // Paper Shredder
+    }
+}
+
+void Enemy_UpdateEnemy(Enemy* enemy) {
+    if (enemy->active == 0) {
+        return;
+    }
+    enemy->x += enemy->spd;
+    enemy->y += enemy->spd;
+}
+
+void Enemy_DrawEnemy(SDL_Renderer* renderer, Enemy* enemy) {
+    if (enemy->active == 0) {
+        return;
+    }
+    SDL_Rect enemyArea = {enemy->x, enemy->y, enemy->w, enemy->h};
+    SDL_RenderCopy(renderer, enemy->sprite, NULL, &enemyArea);
+}
---
+++ Enemy.h
@@ -0,0 +1,42 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include <SDL2/SDL.h>
+
+/* Constants */
+#define MAX_ENEMIES 50 // Max amount of enemies that can be active simultaneously
+extern Enemy enemiesActive[MAX_ENEMIES];
+
+/* Types of enemies */
+typedef enum EnemyType {
+    PENCIL,
+    TELEPHONE,
+    PRINTER,
+    ADHESIVE_TAPE,
+    STAPLER,
+    PAPER_SHREDDER
+} EnemyType;
+
+/* Enemy representation */
+typedef struct Enemy {
+    int type; // Type of enemy
+    int x, y; // Position
+    int w, h; // Dimensions (size)
+    int hp, dmg, def, spd; // Life, damage, defense and speed status
+    SDL_Texture* sprite; // Current sprite
+    int active; // State (dead or alive)
+} Enemy;
+
+/* Enemy creation */
+void Enemy_SpawnEnemy(Enemy* enemy, int enemyType, int enemyPositionX, int enemyPositionY, SDL_Texture* image);
+
+/* Enemy informations update */
+void Enemy_UptadeEnemy(Enemy* enemy);
+
+/* Enemy render */
+void Enemy_DrawEnemy(SDL_Renderer* renderer, Enemy* enemy);
+
+/* Enemy destruction */
+void Enemy_DestroyEnemy();
+
+#endif
