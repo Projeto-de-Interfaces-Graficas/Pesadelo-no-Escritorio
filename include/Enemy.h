182c42caace346c4645135662cf0aaa71afeff45
Daniel Henrique Oliveira <odanielhenrique646@gmail.com>
2025-10-29 10:38:01 -0300

    Change comments in 'Enemy.h'

--- include/Enemy.h
+++ include/Enemy.h
@@ -4,7 +4,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-/* Types of enemies */
 typedef enum EnemyType {
     ENEMY_LAPIS,
     ENEMY_TELEFONE,
@@ -14,27 +13,19 @@ typedef enum EnemyType {
     ENEMY_FRAGMENTADORA
 } EnemyType;
 
-/* Enemy representation */
 typedef struct Enemy {
-    int type; // Type of enemy
-    SDL_Rect box; // Enemy area in-game
-    float posX, posY; // Position X and Y of the enemy 
-    int hp, dmg, def; // Life, damage and defense status
-    float spd; // Movement speed status
-    SDL_Texture* sprite; // Current sprite
-    int active; // State (dead or alive)
+    EnemyType type; 
+    SDL_Rect box; 
+    float posX, posY; 
+    int hp, dmg, def; 
+    float spd; 
+    SDL_Texture* sprite; 
+    int active; 
 } Enemy;
 
-/* Enemy creation */
 void Enemy_CreateEnemy(Enemy* enemy, EnemyType type, float enemyPositionX, float enemyPositionY, SDL_Renderer* ren);
-
-/* Enemy informations update */
 void Enemy_UpdateEnemy(Enemy* enemy, float directionX, float directionY, float deltaTime);
-
-/* Enemy render */
 void Enemy_RenderEnemy(SDL_Renderer* ren, Enemy* enemy);
-
-/* Enemy destruction */
 void Enemy_DestroyEnemy(Enemy* enemy);
 
 #endif
